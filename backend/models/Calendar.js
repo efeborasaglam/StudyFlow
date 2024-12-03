1a81c6d796ed3ddd48525d87b592a7bbb79f4c7e
efeborasaglam <EfeBora.Saglam@hotmail.com>
2024-12-03 16:11:34 +0100

    Delete backend directory

--- backend/models/Calendar.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CalendarSchema = new mongoose.Schema({
-    calendarID: String,
-    userID: String,
-    events: Array
-});
-
-module.exports = mongoose.model('Calendar', CalendarSchema);
